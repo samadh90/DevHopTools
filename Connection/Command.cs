695728922c4fc751aa22955e4b7e7ff46b966be9
Aspirio <soultan_98@hotmail.com>
2022-03-30 12:35:09 +0200

    feat: Added comments

--- Connection/Command.cs
+++ Connection/Command.cs
@@ -5,16 +5,33 @@ namespace DevHopTools.Connection
 {
     public class Command
     {
+        /// <summary>
+        /// The SQL Query or the stored procedure's name
+        /// </summary>
         internal string Query { get; private set; }
+
+        /// <summary>
+        /// Boolean declaration if this <see cref="Command"/> is a Stored Procedure or not
+        /// </summary>
         internal bool IsStoredProcedure { get; private set; }
+
+        /// <summary>
+        /// Dictionnary with key of type <see cref="string"/> and value of type <see cref="Parameter"/>
+        /// </summary>
         internal IDictionary<string, Parameter> Parameters { get; private set; }
 
+        /// <summary>
+        /// Retrieve the Parameter based on its name
+        /// </summary>
+        /// <param name="parameterName">The parameter name from <see cref="Parameters"/></param>
+        /// <returns>A <see cref="Parameter"/> object from <see cref="Parameters"/> dictionnary</returns>
+        /// <exception cref="InvalidOperationException"></exception>
         public object this[string parameterName]
         {
             get
             {
                 if (!Parameters.ContainsKey(parameterName))
-                    throw new InvalidOperationException("the key does not exist!");
+                    throw new InvalidOperationException($"The parameter \"{parameterName}\" does not exist!");
 
                 //if(Parameters[parameterName].Direction == Direction.Input)
                 //    throw new InvalidOperationException("the direction is Input, there no change after call the procedure!");
@@ -23,6 +40,14 @@ namespace DevHopTools.Connection
             }
         }
 
+        /// <summary>
+        /// Create an instance of <see cref="Command"/> object
+        /// </summary>
+        /// <param name="query">The SQL Query or stored procedure's name.</param>
+        /// <param name="isStoredProcedure">
+        /// true If <paramref name="query"/> is a stored procedure. false Otherwise.
+        /// </param>
+        /// <exception cref="ArgumentException"></exception>
         public Command(string query, bool isStoredProcedure = false)
         {
             if (string.IsNullOrWhiteSpace(query))
@@ -33,11 +58,24 @@ namespace DevHopTools.Connection
             Parameters = new Dictionary<string, Parameter>();
         }
 
+        /// <summary>
+        /// Add a parameter with it's key and value in the <see cref="Parameters"/> dictionnary
+        /// </summary>
+        /// <param name="parameterName">Parameter's name for the query</param>
+        /// <param name="value">Parameter's value for the query</param>
         public void AddParameter(string parameterName, object value)
         {
             AddParameter(parameterName, value, Direction.Input);
         }
 
+        /// <summary>
+        /// Add a parameter with it's key, its value and the direction
+        /// </summary>
+        /// <param name="parameterName">Parameter key</param>
+        /// <param name="value">Parameter's value</param>
+        /// <param name="direction">SQL direction. It query is not stored procedure and direction is output,
+        /// an exception is thrown.</param>
+        /// <exception cref="ArgumentException"></exception>
         public void AddParameter(string parameterName, object value, Direction direction)
         {
             if (string.IsNullOrWhiteSpace(parameterName))
